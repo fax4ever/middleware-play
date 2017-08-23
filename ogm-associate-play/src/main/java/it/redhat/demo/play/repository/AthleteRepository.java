e9ec10bfbd011d9ec07a875ca5042d7eed38f866
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-08-23 09:44:53 +0200

    hibernate ogm test with multi container arquillian (wildfly + infinispan server)

---
+++ ogm-associate-play/src/main/java/it/redhat/demo/play/repository/AthleteRepository.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package it.redhat.demo.play.repository;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import it.redhat.demo.play.entity.Athlete;
+import it.redhat.demo.play.entity.Club;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Fabio Massimo Ercoli (C) 2017 Red Hat Inc.
+ */
+@Stateless
+public class AthleteRepository {
+
+    private final static Logger LOG = LoggerFactory.getLogger(AthleteRepository.class);
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Athlete createAthlete(Athlete athlete, Club club) {
+
+        if ( club != null ) {
+            club = entityManager.merge( club );
+            athlete.club = club;
+            club.athletes.add( athlete );
+        }
+
+        entityManager.persist( athlete );
+        return athlete;
+    }
+
+    public Athlete saveAthlete(Athlete athlete) {
+        entityManager.persist( athlete );
+        return athlete;
+    }
+
+    public Club createClub(Club club) {
+        entityManager.persist( club );
+        return club;
+    }
+
+    public Athlete getAthleteById(long athleteId) {
+        return entityManager.find( Athlete.class, athleteId );
+    }
+
+    public Club getClubById(long clubId) {
+        return entityManager.find( Club.class, clubId );
+    }
+
+    public Club getClubByIdWithAthletes(long clubId) {
+        Club club = entityManager.find(Club.class, clubId);
+
+        LOG.info("Club \"{}\" has {} athletes!", club.code, club.athletes.size());
+        return club;
+    }
+
+    public Athlete addAthleteToClub(Athlete athlete, Club club) {
+
+        club = entityManager.merge(club);
+        athlete = entityManager.merge(athlete);
+
+        athlete.club = club;
+        club.athletes.add( athlete );
+
+        return athlete;
+
+    }
+
+}
---
+++ ogm-associate-play/src/test/java/it/redhat/demo/play/it/AthleteIT.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package it.redhat.demo.play.it;
+
+import java.util.List;
+import javax.inject.Inject;
+
+import it.redhat.demo.play.entity.Athlete;
+import it.redhat.demo.play.entity.Club;
+import it.redhat.demo.play.repository.AthleteRepository;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Fabio Massimo Ercoli (C) 2017 Red Hat Inc.
+ */
+@RunWith(Arquillian.class)
+public class AthleteIT {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AthleteIT.class);
+
+    @Deployment
+    public static WebArchive create() {
+
+        return ShrinkWrap.create(WebArchive.class)
+            .addPackages(true, "it.redhat.demo.play")
+            .addAsResource("META-INF/persistence.xml")
+            .addAsResource("hotrodclient.properties")
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+            .addAsWebInfResource("jboss-deployment-structure.xml");
+
+    }
+
+    @Inject
+    private AthleteRepository repository;
+
+    @Test
+    public void test_createClub_createLinkedAthlete() {
+
+        Club club = new Club();
+        club.code = "RM739";
+
+        repository.createClub(club);
+
+        Athlete athlete = new Athlete();
+        athlete.uispCode = "123456789";
+
+        repository.createAthlete(athlete, club);
+
+        // reload entities
+        long clubId = club.id;
+        long athleteId = athlete.id;
+
+        // verify loaded trip
+        club = repository.getClubByIdWithAthletes(clubId);
+        LOG.info("Loaded CLUB {}", club);
+
+        assertNotNull(club);
+        assertEquals(clubId, club.id);
+        assertEquals("RM739", club.code);
+
+        List<Athlete> athletes = club.athletes;
+        assertEquals(1, athletes.size());
+
+        Athlete linkedAthlete = athletes.iterator().next();
+        assertEquals(athleteId, linkedAthlete.id);
+        assertEquals("123456789", linkedAthlete.uispCode);
+
+        // verify loaded hike
+        athlete = repository.getAthleteById(athleteId);
+        LOG.info("Loaded ATHLETE {}", athlete);
+
+        assertNotNull(athlete);
+        assertEquals(athleteId, athlete.id);
+        assertEquals("123456789", athlete.uispCode);
+
+        Club linkedClub = athlete.club;
+
+        assertNotNull(linkedClub);
+        assertEquals(clubId, linkedClub.id);
+        assertEquals("RM739", linkedClub.code);
+
+    }
+
+    @Test
+    public void test_createClub_createAthlete_associate() {
+
+        Club club = new Club();
+        club.code = "RM739";
+
+        repository.createClub(club);
+
+        Athlete athlete = new Athlete();
+        athlete.uispCode = "123456789";
+
+        repository.saveAthlete(athlete);
+
+        repository.addAthleteToClub(athlete, club);
+
+        // reload entities
+        long clubId = club.id;
+        long athleteId = athlete.id;
+
+        // verify loaded trip
+        club = repository.getClubByIdWithAthletes(clubId);
+        LOG.info("Loaded CLUB {}", club);
+
+        assertNotNull(club);
+        assertEquals(clubId, club.id);
+        assertEquals("RM739", club.code);
+
+        List<Athlete> athletes = club.athletes;
+        assertEquals(1, athletes.size());
+
+        Athlete linkedAthlete = athletes.iterator().next();
+        assertEquals(athleteId, linkedAthlete.id);
+        assertEquals("123456789", linkedAthlete.uispCode);
+
+        // verify loaded hike
+        athlete = repository.getAthleteById(athleteId);
+        LOG.info("Loaded ATHLETE {}", athlete);
+
+        assertNotNull(athlete);
+        assertEquals(athleteId, athlete.id);
+        assertEquals("123456789", athlete.uispCode);
+
+        Club linkedClub = athlete.club;
+
+        assertNotNull(linkedClub);
+        assertEquals(clubId, linkedClub.id);
+        assertEquals("RM739", linkedClub.code);
+
+    }
+
+}
