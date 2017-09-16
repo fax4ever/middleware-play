fbc75c5d54c769858161122bb820b5949baed3a5
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-09-16 22:19:53 +0200

    testing WriteSkewCheck and FineGrainedAtomicMap on Infinispan 9.1.0

--- infinispan-local/src/test/java/it/redhat/demo/infinispan/standalone/SimulateConcurrencyHelper.java
+++ infinispan-local/src/test/java/it/redhat/demo/infinispan/standalone/SimulateConcurrencyHelper.java
@@ -1,19 +1,12 @@
 package it.redhat.demo.infinispan.standalone;
 
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
-import javax.transaction.TransactionManager;
 
+import it.redhat.demo.infinispan.atomic.AtomicMapConcurrentUpdate;
+import it.redhat.demo.infinispan.simple.CacheMapConcurrentUpdate;
 import org.infinispan.Cache;
-import org.infinispan.atomic.AtomicMapLookup;
-import org.infinispan.atomic.FineGrainedAtomicMap;
 
 /**
  * @author Fabio Massimo Ercoli (C) 2017 Red Hat Inc.
@@ -22,53 +15,13 @@ public class SimulateConcurrencyHelper {
 
     public static Future<?> updateEntryInConcurrentThread(Cache cache, String key, String subKey, int value) {
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-
-        return executor.submit(() -> {
-
-            TransactionManager tm = cache.getAdvancedCache().getTransactionManager();
-            try {
-                tm.begin();
-            } catch (NotSupportedException | SystemException e) {
-                throw new RuntimeException(e);
-            }
-
-            FineGrainedAtomicMap<String, Integer> fineGrainedAtomicMap = AtomicMapLookup.getFineGrainedAtomicMap(cache, key);
-            fineGrainedAtomicMap.put(subKey, value);
-
-            try {
-                tm.commit();
-            } catch (RollbackException | HeuristicMixedException | HeuristicRollbackException | SystemException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        return Executors.newSingleThreadExecutor().submit(new AtomicMapConcurrentUpdate(cache, key, subKey, value));
 
     }
 
     public static Future<?> updateMapInConcurrentThread(Cache<String, Map<String, Integer>> cache, String key, String subKey, int value) {
 
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-
-        return executorService.submit(() -> {
-
-            TransactionManager tm = cache.getAdvancedCache().getTransactionManager();
-            try {
-                tm.begin();
-            } catch (NotSupportedException | SystemException e) {
-                throw new RuntimeException(e);
-            }
-
-            Map<String, Integer> entry = cache.get(key);
-            entry.put(subKey, value);
-            cache.put(key, entry);
-
-            try {
-                tm.commit();
-            } catch (RollbackException | HeuristicMixedException | HeuristicRollbackException | SystemException e) {
-                throw new RuntimeException(e);
-            }
-
-        });
+        return Executors.newSingleThreadExecutor().submit(new CacheMapConcurrentUpdate(cache, key, subKey, value));
 
     }
 
