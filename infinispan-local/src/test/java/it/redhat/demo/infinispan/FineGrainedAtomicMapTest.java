f3e006995a02411ee60d9f181c55de67c1942940
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-09-16 15:18:04 +0200

    FineGrainedAtomicMap behaviour in cache entities on Inifinispan 9.1

--- infinispan-local/src/test/java/it/redhat/demo/infinispan/FineGrainedAtomicMapTest.java
+++ infinispan-local/src/test/java/it/redhat/demo/infinispan/FineGrainedAtomicMapTest.java
@@ -10,11 +10,11 @@ import org.infinispan.Cache;
 import org.infinispan.atomic.AtomicMapLookup;
 import org.infinispan.atomic.FineGrainedAtomicMap;
 import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.stream.CacheCollectors;
 import org.junit.Test;
 
 import static it.redhat.demo.infinispan.DistCacheTest.INFINISPAN_DIST_XML;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * @author Fabio Massimo Ercoli (C) 2017 Red Hat Inc.
@@ -23,7 +23,7 @@ public class FineGrainedAtomicMapTest {
 
     @Test
     public void test() throws Exception {
-        Cache<String, Map<String, String>> cache = new DefaultCacheManager(INFINISPAN_DIST_XML).getCache("DEFAULT");
+        Cache<String, Map<String, String>> cache = new DefaultCacheManager(INFINISPAN_DIST_XML).getCache("ENTITY");
 
         final String key = "mainKey";
 
@@ -36,8 +36,8 @@ public class FineGrainedAtomicMapTest {
         fineGrainedAtomicMap.put("k3", "v3");
 
         tm.commit();
-
-        List<Map.Entry<String, Map<String, String>>> collect = cache.entrySet().stream().collect(Collectors.toList());
+        List<Map.Entry<String, Map<String, String>>> collect = cache.entrySet().stream()
+            .collect(CacheCollectors.serializableCollector(() -> Collectors.toList()));
 
         assertNotNull(collect);
         assertEquals(4, collect.size());
